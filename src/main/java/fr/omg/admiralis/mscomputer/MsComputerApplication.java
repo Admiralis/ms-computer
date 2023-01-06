4f46f29145e1e6f8249c51e041f9ad00a84dd19d
0h My Goat <daftouney@gmail.com>
2023-01-06 16:09:46 +0100

    chore: init project :tata:

---
+++ src/main/java/fr/omg/admiralis/mscomputer/MsComputerApplication.java
@@ -0,0 +1,13 @@
+package fr.omg.admiralis.mscomputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsComputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsComputerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/omg/admiralis/mscomputer/MsComputerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.omg.admiralis.mscomputer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsComputerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
