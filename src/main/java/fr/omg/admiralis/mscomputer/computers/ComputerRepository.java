40d24d5a96fcb29e6a9e11d0a1b71b4c291a8a49
0h My Goat <daftouney@gmail.com>
2023-05-10 14:47:14 +0200

    feat: implémentation logique prêts :sparkles:

--- src/main/java/fr/omg/admiralis/mscomputer/computers/ComputerRepository.java
+++ src/main/java/fr/omg/admiralis/mscomputer/computers/ComputerRepository.java
@@ -1,5 +1,6 @@
 package fr.omg.admiralis.mscomputer.computers;
 
+import jakarta.websocket.MessageHandler;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ComputerRepository extends MongoRepository<Computer, String> {
