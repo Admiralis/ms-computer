11d4d034dab52a0bb211f02212ddc7462a636d2e
0h My Goat <daftouney@gmail.com>
2023-05-11 14:16:13 +0200

    fix: update computer :bug:

--- src/main/java/fr/omg/admiralis/mscomputer/computers/ComputerService.java
+++ src/main/java/fr/omg/admiralis/mscomputer/computers/ComputerService.java
@@ -80,24 +80,24 @@ public class ComputerService {
     }
 
     public Computer update(String id, Computer updateComputer) {
-        Computer computer = findById(id);
-        if (updateComputer.getSerialNumber() != null) {
-            computer.setSerialNumber(updateComputer.getSerialNumber());
-        }
-        if (updateComputer.getProcessor() != null) {
-            computer.setProcessor(updateComputer.getProcessor());
-        }
-        if (updateComputer.getRam() != null) {
-            computer.setRam(updateComputer.getRam());
-        }
-        if (updateComputer.getCondition() != null) {
-            computer.setCondition(updateComputer.getCondition());
-        }
-        if (updateComputer.getComputerStatus() != null) {
-            computer.setComputerStatus(updateComputer.getComputerStatus());
-        }
-        computerRepository.save(computer);
-        return findById(computer.getId());
+//        Computer computer = findById(id);
+//        if (updateComputer.getSerialNumber() != null) {
+//            computer.setSerialNumber(updateComputer.getSerialNumber());
+//        }
+//        if (updateComputer.getProcessor() != null) {
+//            computer.setProcessor(updateComputer.getProcessor());
+//        }
+//        if (updateComputer.getRam() != null) {
+//            computer.setRam(updateComputer.getRam());
+//        }
+//        if (updateComputer.getCondition() != null) {
+//            computer.setCondition(updateComputer.getCondition());
+//        }
+//        if (updateComputer.getComputerStatus() != null) {
+//            computer.setComputerStatus(updateComputer.getComputerStatus());
+//        }
+        computerRepository.save(updateComputer);
+        return findById(id);
     }
 
     public Computer findBySerialNumber(String serialNumber) {
